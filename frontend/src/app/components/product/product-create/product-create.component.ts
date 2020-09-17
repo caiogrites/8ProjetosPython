d8ff5bf2b71942baafee24ef65b202f5e8c97878
Caio Grites <caiogrites@gmail.com>
2020-09-17 16:06:02 -0300

    first commit

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,35 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  Product: Product = {
+    name:'',
+    price: null
+  }
+
+  constructor(private ProductService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  createProduct(): void{
+    this.ProductService.create(this.Product).subscribe(() => {
+      this.ProductService.showMessage('Produto criado!');
+        this.router.navigate(['/products'])
+    })
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
